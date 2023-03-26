3ae1965075d9e94da0c95a5bee924d0d9c93b363
Vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-03-26 23:20:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function createTable() {
-    //Write your code here
+    rn = window.prompt("Input number of rows");
+    cn = window.prompt("Input number of columns");
   
+    for(var r=0;r<parseInt(rn);r++) {
+        var x=document.getElementById('myTable').insertRow(r);
+        for(var c=0;c<parseInt(cn);c++) {
+            var y=  x.insertCell(c);
+            y.innerHTML="Row-"+r+" Column-"+c; 
+        }
+    }
 }
+  
